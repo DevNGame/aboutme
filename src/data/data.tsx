4c1c127c32ff1fd0746d6bf810ffe46493cf2b5a
Sahil Gupta <sahilgup@adobe.com>
2026-03-13 01:15:13 +0530

    fix resume url

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -75,7 +75,7 @@ export const heroData: Hero = {
   ),
   actions: [
     {
-      href: '//resume.pdf',
+      href: 'resume.pdf',
       text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
